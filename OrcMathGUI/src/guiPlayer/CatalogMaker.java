1b6d63594846e2115c084aab66dc87dbe8e61a61
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-12-20 14:22:48 -0500

    wat

--- OrcMathGUI/src/guiPlayer/CatalogMaker.java
+++ OrcMathGUI/src/guiPlayer/CatalogMaker.java
@@ -1,176 +1,37 @@
 package guiPlayer;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CatalogMaker {
 
-	public static Scanner in;
-
-	private ArrayList<Book> catalog;
-
+	private ArrayList<Pizza> catalog;
+	
 	public CatalogMaker() {
-		//instantiate the catalog
-		catalog = new ArrayList<Book>();
+		catalog = new ArrayList<Pizza>();
+		catalog.add(new Pizza("Order 1", "Regular", "Pepperoni", 3.50, 1));
+		catalog.add(new Pizza("Order 2", "Sicilian", "Pepperoni", 3.50, 1));
+		catalog.add(new Pizza("Order 3", "Regular", "Pepperoni", 3.50, 1));
+		catalog.add(new Pizza("Order 4", "Sicilian", "Pepperoni", 3.50, 1));
 	}
 
-	public static void main(String[] args){
-		CatalogMaker maker = new CatalogMaker();
-		in = new Scanner(System.in);
-		maker.menu();
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		CatalogMaker c = new CatalogMaker();
+		System.out.println(c.getCSVContent());
 	}
 
-	private static void displayMessage(String message){
-		System.out.println(message);
-	}
-
-	private void menu() {
-		displayMessage("Would you like to \"load\" a save file or \"create\" a new list? ");
-		String[] allowedEntry = {"load","create"};
-		String input = getEntry(allowedEntry);
-		if(input.equals("load")){
-			load();
-		}else{
-			create();
+	public String getCSVContent() {
+		String data = "orderNumber, type, toppings, price, amount\n";
+		for(Pizza p: catalog) {
+			data += p + "\n";
 		}
+		return data;
 	}
-
-	private void create() {
-		
-		boolean running = true;
-		while(running){
-			showCatalog();
-			displayMessage("Would you like to \"add\", \"save\", or \"quit\"?");
-			String[] allowedEntry = {"add","save","quit"};
-			String selection = getEntry(allowedEntry);
-			if(selection.equals("add")){
-				add();
-			}else if(selection.equals("save")){
-				save();
-			}else{
-				running = false;
-			}
-		}
+	
+	public void addNewItem(String orderNumber, String type, String toppings, double price, int amount) {
+		catalog.add(new Pizza(orderNumber, type, toppings, price, amount));
+		System.out.println("Item added successfully!");
 	}
-
-	private void add() {
-		String title = null;
-		String author = null;
-		int pages = 0;
-		displayMessage("Please enter a title");
-		title = getStringInput();
-		displayMessage("Please enter an author");
-		author = getStringInput();
-		displayMessage("Please enter the number of pages.");
-		pages = getIntegerInput();
-		addBook(new Book(title, author, pages));
-	}
-
-	private int getIntegerInput() {
-		String text = in.nextLine();
-		int value = 0;
-		boolean valid = false;
-		while(!valid){
-			try{
-				value = Integer.parseInt(text);
-				valid = true;
-			}catch(NumberFormatException nfe){
-				displayMessage("You must enter an integer.");
-			}
-		}
-		return value;
-	}
-
-	private static String getStringInput(){
-		String text = in.nextLine();
-		while(text.isEmpty()){
-			displayMessage("You must enter a non-empty String.");
-			text = in.nextLine();
-		}
-		return text;
-	}
-
-
-	private void addBook(Book b){
-		catalog.add(b);
-	}
-
-	public void save() {
-		try{    
-			FileWriter fw = new FileWriter("BookCatalog.csv");
-			for(Book b: catalog){
-				fw.write(b+"\n");    	
-			}
-
-			fw.close();    
-			System.out.println("Success! File \"BookCatalog.csv\" saved!");
-		}catch(IOException e){
-			System.out.println("An IOException was thrown. \nCheck to see that the directory where you tried to save the file actually exists.");
-		}
-	}
-
-	private static String getEntry(String[] allowedEntry) {
-		String input = in.nextLine();
-		while(!matchesEntry(input, allowedEntry)){
-			displayMessage("You must enter one of these words: ");
-			for(String s: allowedEntry){
-				System.out.print(s+" ");
-			}
-			displayMessage("\n");
-			input = in.nextLine();
-		}
-		return input;
-	}
-
-	private static boolean matchesEntry(String text, String[] list){
-		for(String l: list){
-			if(l.equals(text))return true;
-		}
-		return false;
-	}
-
-	private void showCatalog() {
-		displayMessage("The catalog contains these Books:\n");
-		for(Book b: catalog){
-			displayMessage("   "+b.toString()+"\n");
-		}
-	}
-
-	private void load() {
-		String fileName = "";
-		//empty the catalog to prepare for a new load
-		catalog = new ArrayList<Book>();
-		//use this boolean to control the while loop. The user should have multiple chances to enter a correct filename
-		boolean opened = false;
-		while(!opened){
-			try {
-				System.out.println("Enter a file to open");
-				fileName = in.nextLine();
-				FileReader fileReader = new FileReader(new File(fileName));
-				String line = "";
-				//a BufferedReader enables us to read teh file one line at a time
-				BufferedReader br = new BufferedReader(fileReader);
-				while ((line = br.readLine()) != null) {
-
-					String[] param = line.split(",");
-					//add a new Book for each line in the save file
-					catalog.add(new Book(param[0],param[1],Integer.parseInt(param[2])));
-
-
-
-				}
-				br.close();
-				opened = true;
-			}catch (IOException e) {
-				System.out.println("The file name you specified does not exist.");
-			}
-		}
-		create();
-
-	}
+	
 }
