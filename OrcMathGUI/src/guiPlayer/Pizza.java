55dd7b7ee06a7260cc29c5f598ba3075b617dc90
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-12-20 14:15:15 -0500

    yeety

--- OrcMathGUI/src/guiPlayer/Pizza.java
+++ OrcMathGUI/src/guiPlayer/Pizza.java
@@ -1,5 +1,44 @@
 package guiPlayer;
 
-public class Pizza {
+import java.awt.Color;
+import java.awt.Graphics2D;
 
+import guiTeacher.components.AnimatedComponent;
+import guiTeacher.components.Component;
+
+public class Pizza extends AnimatedComponent {
+
+	private String orderNumber;
+	private String type;
+	private String toppings;
+	private double price;
+	private int amount;
+	
+	public Pizza(String orderNumber, String type, String toppings, double price, int amount) {
+		super(40, 40, 100, 100);
+		//addSequence("resources/sheet.png", 150, 0, 0, 89, 175, 5);
+		//Thread animation = new Thread(this);
+		//animation.start();
+		
+		this.orderNumber = orderNumber;
+		this.type = type;
+		this.toppings = toppings;
+		this.price = price;
+		this.amount = amount;
+		
+		update();
+		
+	}
+
+	
+	public void update(Graphics2D g) {
+	//	g.setColor(Color.PINK);
+	//	g.fillRect(0, 0, getWidth(), getHeight());
+	//	super.update(g);
+	}
+
+	public String toString() {
+		return orderNumber + "," + type + "," + toppings + "," + price + "," + amount;
+	}
+	
 }
