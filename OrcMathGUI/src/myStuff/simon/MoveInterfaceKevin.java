e446603ba919a64f09f9bac05c08a0badab2b7ee
KevinLiu0862 <31863360+KevinLiu0862@users.noreply.github.com>
2018-01-08 07:50:30 -0500

    Update MoveInterfaceKevin.java

--- OrcMathGUI/src/myStuff/simon/MoveInterfaceKevin.java
+++ OrcMathGUI/src/myStuff/simon/MoveInterfaceKevin.java
@@ -1,5 +1,10 @@
 package myStuff.simon;
 
 public interface MoveInterfaceKevin {
-
+  static ButtonInterfaceKevin getButton() {
+	  	// TODO Auto-generated method stub
+  		return null;
+  	}
+   	Object get(int sequenceIndex);
+  	ButtonInterfaceKevin getAButton();
 }
